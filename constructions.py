82e00a92fc2ec1dd9676fd8ca2f571f6a8b63f3c
Simon Heidrich <92866829+smh0109@users.noreply.github.com>
2021-12-30 19:31:49 +0100

    added triangleIsCovered to constructions.py few changes to test_code_drawing.ipynb and test_code_deltaslim.ipynb

--- constructions.py
+++ constructions.py
@@ -12,32 +12,51 @@ def deltaLines_of_Line(Line, offset):
 
 def is_on_Linesegment(px,py, Line):
     ''' Assumes that the given point is on the line '''
-    assert Line.segment
-    assert isinstance(Line.projShape, ( Arc, ELine))    #check wheter Line has segment
+    assert isinstance(Line.projShape, (Arc, ELine))    #check wheter Line has segment
     if isinstance(Line.projShape, ELine): 
-        K1=(Line.projShape.x1-px)*(Line.projShape.x1-Line.projShape.x2)+(Line.projShape.y1-py)*(Line.projShape.y1-Line.projShape.y2)
-        K2=(Line.projShape.x2-px)*(Line.projShape.x2-Line.projShape.x1)+(Line.projShape.y2-py)*(Line.projShape.y2-Line.projShape.y1)
-        if K1>=0 and K2>=0:     #do we need > ?
+        K1=(px-Line.projShape.x1)*(Line.projShape.x2-Line.projShape.x1)+(py-Line.projShape.y1)*(Line.projShape.y2-Line.projShape.y1)
+        K2=(px-Line.projShape.x2)*(Line.projShape.x1-Line.projShape.x2)+(py-Line.projShape.y2)*(Line.projShape.y1-Line.projShape.y2)
+        if K1>=0 and K2>=0:
             return True
         else:
             return False
     elif isinstance(Line.projShape,Arc):
         px = px-Line.projShape.cx
         py = py-Line.projShape.cy
-        pDeg = math.degrees(math.atan2(py, px))
+        pDeg = math.degrees(math.atan2(py, px))%360
         if not Line.projShape.cw:
-            return Line.projShape.startDeg<=pDeg and pDeg<=Line.projShape.endDeg    #do we need < ?
+            return Line.projShape.startDeg%360<=pDeg and pDeg<=Line.projShape.endDeg%360    #do we need %360 ?
         else:
-            return Line.projShape.startDeg>=pDeg and pDeg>=Line.projShape.endDeg    #do we need > ?
+            return Line.projShape.startDeg%360>=pDeg and pDeg>=Line.projShape.endDeg%360    #do we need %360 ?
 
 
-def ips_with_deltaneigbourhood(sideLine,deltaLines,delta):
-    PL=[]
-    for l in deltaLines:
-        for h in deltaLines_of_Line(l,delta):
-            px1,py1,px2,py2=intersection.circleCircle(h.projShape,sideLine.projShape)
-            if is_on_Linesegment(px1,py1,sideLine):
-                PL.append(Point.fromEuclid(px1,py1))
-            elif is_on_Linesegment(px2,py1,sideLine):
-                PL.append(Point.fromEuclid(px2,py2))
-    return PL
+def triangleIsCovered(triangle, delta):
+    assert isinstance(triangle,Polygon)
+    edges=triangle.edges
+    for i, edge in enumerate(edges):
+        side1, side2 = edges[(i-1)%len(edges)], edges[(i+1)%len(edges)]
+        s1 = edge.intersectionsWithHcycle(side1)
+        p1, p2 = [], []
+        for h1 in deltaLines_of_Line(side1, delta):
+            ip1 = edge.intersectionsWithHcycle(h1)
+            for p in ip1:
+                if is_on_Linesegment(*p,edge):
+                    p1.append(p)
+        for h2 in deltaLines_of_Line(side2, delta):
+            ip2 = edge.intersectionsWithHcycle(h2)
+            for p in ip2:
+                if is_on_Linesegment(*p,edge):
+                    p2.append(p)
+        if len(p1) <= 0 or len(p2) <= 0:
+            continue
+        elif len(p1) > 1 or len(p2) > 1:
+            raise Exception('Intersection with edge %s is ambiguous'%i)
+        else:
+            if is_on_Linesegment(*p2, Line.fromPoints(*s1, *p1, segment=True)):
+                continue
+            else:
+                return False
+    
+
+
+
