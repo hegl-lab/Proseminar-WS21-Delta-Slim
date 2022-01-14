71dd61c43624bcbcedf442cdf1e8ad932d3998ec
Simon Heidrich <92866829+smh0109@users.noreply.github.com>
2022-01-14 04:21:39 +0100

    fixed Triangle.isCovered(self.delta) for IdealPoints added new raw functions to deltaNeigbourhood.py

--- Triangle.py
+++ Triangle.py
@@ -1,41 +1,73 @@
+import math
 from hyperbolic.poincare.shapes import *
 from constructions import deltaLines_of_Line, isPointOnSegment
 
 class Triangle(Polygon):
     def __init__(self, edges=None, join=False, vertices=None):
+        if not edges==None:
+            assert len(edges)==3
+        elif not vertices==None:
+            assert len(vertices)==3
         super().__init__(edges=edges, join=join, vertices=vertices)
+    def isCCW(self):
+        x0, y0 = self.vertices[0].x, self.vertices[0].y
+        x1, y1 = self.vertices[1].x, self.vertices[1].y
+        x2, y2 = self.vertices[2].x, self.vertices[2].y
+        Deg1 = math.degrees(math.atan2(y0, x0))
+        Deg2 = math.degrees(math.atan2(y1, x1))
+        Deg3 = math.degrees(math.atan2(y2, x2))
+        cw = (Deg2-Deg1)%360 > (Deg3-Deg1)%360
+        return not cw
+    def isIdeal(self):
+        return (self.vertices[0].isIdeal() and self.vertices[1].isIdeal() and self.vertices[2].isIdeal())
+    def isEdgeIdeal(self, edgeNum):
+        if (self.vertices[(edgeNum+1)%len(self.vertices)].isIdeal() 
+            and self.vertices[edgeNum%len(self.vertices)].isIdeal()):
+            return True
+        else:
+            return False
+    def offsetEdge(self, edgeNum, offset, inner=True):
+        if ((self.isCCW() and offset<=0) 
+            or (not self.isCCW() and offset>=0)):
+            offset = -offset
+        if self.isEdgeIdeal(edgeNum=edgeNum):
+            offset = -offset
+        if inner:
+            return Hypercycle.fromHypercycleOffset(self.edges[edgeNum%len(self.edges)], offset)
+        else:
+            return Hypercycle.fromHypercycleOffset(self.edges[edgeNum%len(self.edges)], -offset)
     def isCovered(self, delta):
+        if (self.isCCW() and delta<=0) or (not self.isCCW() and delta>=0):
+            delta = -delta
         for i, edge in enumerate(self.edges):
-            side1, side2 = self.edges[(i-1)%len(self.edges)], self.edges[(i+1)%len(self.edges)]
-            vert = edge.intersectionsWithHcycle(side1)
-            p1, p2 = [], []
-            for h1 in deltaLines_of_Line(side1, delta):
-                ip1 = edge.intersectionsWithHcycle(h1)
-                for p in ip1:
-                    if isPointOnSegment(edge, *p):
-                        p1.append(p)
-            for h2 in deltaLines_of_Line(side2, delta):
-                ip2 = edge.intersectionsWithHcycle(h2)
-                for p in ip2:
-                    if isPointOnSegment(edge, *p):
-                        p2.append(p)
-            if len(p1) <= 0 or len(p2) <= 0:        #one side delta is sourrounding the whole edge
+            ip1 = edge.intersectionsWithHcycle(self.offsetEdge(i-1,delta))
+            ip2 = edge.intersectionsWithHcycle(self.offsetEdge(i+1,delta))
+            p1 = [p for p in ip1 if (isPointOnSegment(edge, *p) and not p.isIdeal())]   #tiny delta for ideal triangles will remove all points
+            p2 = [p for p in ip2 if (isPointOnSegment(edge, *p) and not p.isIdeal())]   #tiny delta for ideal triangles will remove all points
+            if len(p1) <= 0 or len(p2) <= 0:            #deltaNbh of one side is sourrounding the whole edge or problem above
                 continue
             elif len(p1) > 1 or len(p2) > 1:
-                #TODO: if of the Points is an Ideal remove this Point
-                raise Exception('Intersection with edge %s is ambiguous'%i)
+                raise ValueError('Intersection with edge %s is ambiguous'%i)
             else:
                 ps1, ps2 = p1[0], p2[0]
-                s1=vert[0]
+                s1 = self.vertices[i]
                 if isPointOnSegment(Line.fromPoints(*s1, *ps1, segment=True), *ps2):
                     continue
                 else:
                     return False
         return True
-    def isCw(self):
-        #TODO: return True if triangle is clockwise, 'cause only inner hypercycle is needed for isCovered
-        # can be decided by CW or CCW 
-        pass
+    def offsetTriangle(self, offset, reverseOrder=False):
+        ''' If self is a CCW triangle, returns a CCW triangle that is smaller by
+            offset. '''
+        edges = self.edges
+        if reverseOrder:
+            edges = reversed(edges)
+        offEdges = [  # Maybe outer, maybe inner
+            edge.makeOffset(offset)
+            for edge in edges
+            if not isinstance(edge, Point) or edge.isIdeal()
+        ]
+        return Triangle.fromEdges(offEdges, join=True)
     def makeRandom(self):
         #TODO: returns random triangle
         pass
@@ -44,3 +76,6 @@ class Triangle(Polygon):
         if len(vertices)!=3:
             raise ValueError('Triangle vertices need to be three Points')
         return cls(vertices=vertices)
+    @classmethod
+    def fromEdges(cls, edges, join=True):
+        return cls(edges=edges, join=join)
--- deltaNeigbourhood.py
+++ deltaNeigbourhood.py
@@ -27,7 +27,56 @@ def makeRoundCap(hcycle, point, delta):
         arc = Arc.fromPoints(*p1, *p2, *mp, excludeMid=True)
         return arc                  #we need Line(arc, segment=True) but this implementation is not yet in hyperbolic
 
+def makeEndCap(triangle, vertNum, edgeNum, delta):
+    '''Not tested yet'''
+    assert edgeNum%len(triangle.vertices)!=(vertNum+1)%len(triangle.vertices) #edge and vertice don't lie opposide of each other
+    point=triangle.vertices[vertNum%len(triangle.vertices)]
+    hcycle=triangle.edges[edgeNum%len(triangle.edges)]
+    if point.isIdeal():
+        return point
+    else:
+        perp = hcycle.makePerpendicular(*point)
+        points1 = perp.intersectionsWithHcycle(hcycle.makeOffset(delta))
+        points2 = perp.intersectionsWithHcycle(hcycle.makeOffset(-delta))
+        mpoints = []
+        for h in deltaLines_of_Line(perp, delta):
+            for p in hcycle.intersectionsWithHcycle(h):
+                if isPointOnSegment(hcycle, *p):
+                    mpoints.append(p)
+        assert len(points1)==1 and len(points2)==1 and len(mpoints)==1
+        p1, p2, mp = points1[0], points2[0], mpoints[0]
+        arc = Arc.fromPoints(*p1, *p2, *mp, excludeMid=True)
+        return arc
+
+def innerInterPoint(triangle, vertNum, delta):
+    '''Not tested yet'''
+    ips=triangle.offsetEdge(vertNum-1).intersectionsWithHcycle(triangle.offsetEdge(vertNum))
+    #TODO determine valid ip out of ips
+    pass
+
+def makeMidCap(triangle, vertNum, delta):
+    '''Not tested yet'''
+    point=triangle.vertices[vertNum%len(triangle.vertices)]
+    side1, side2 =triangle.edges[(vertNum-1)%len(triangle.vertices)], triangle.edges[vertNum%len(triangle.vertices)]
+    perp1, perp2 = side1.makePerpendicular(*point), side2.makePerpendicular(*point)
+    p1=perp1.intersectionsWithHcycle(triangle.offsetEdge(vertNum-1, offset=delta, inner=False))
+    p2=perp2.intersectionsWithHcycle(triangle.offsetEdge(vertNum, ofset=delta, inner=False))
+    circ=Circle.fromCenterRadius(point, delta)
+    arc=Arc.fromPointsWithCenter(*p1,*p2,circ.projShape.cx, circ.projShape.y, r=circ.projShape.r)
+    #arc.reverse() if wrong part of the arc
+    return arc
 
 
+lineStyle1 = dict(fill='red', fill_opacity=1)
+
+def deltaNbh(triangle, drawing, delta, edgeNum=0):
+    '''old needs change with def from above'''
+    for i,l in enumerate(triangle.edges):
+        if i==edgeNum:
+            continue
+        else:
+            drawing.draw(makeRoundCap(l, triangle.vertices[i], delta), **lineStyle1)
+            drawing.draw(makeRoundCap(l, triangle.vertices[(i+1)%len(triangle.vertices)], delta), **lineStyle1)
+            drawing.draw(l, hwidth=2*delta, **lineStyle1)
 
 
